ef7cb325700dab2d20f7502de1ec541c4dbfcaed
xiaodan cui <cxd@xiaodan-MacBook-Pro.local>
2021-01-17 22:22:34 -0800

    forth commit

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,20 +1,18 @@
-
-
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10', // stylesheet location
-    center: camp.geometry.coordinates, // starting position [lng, lat]
-    zoom: 8
-    // starting zoom
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v10", // stylesheet location
+  center: camp.geometry.coordinates, // starting position [lng, lat]
+  zoom: 8,
+  // starting zoom
 });
 
-const marker=new mapboxgl.Marker()
-    .setLngLat(camp.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({offset:25})
-        .setHTML(
-            `<h3>${camp.title}</h3><p>${camp.location}</p>`
-        )
+const marker = new mapboxgl.Marker()
+  .setLngLat(camp.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${camp.title}</h3><p>${camp.location}</p>`
     )
-    .addTo(map)
+  )
+  .addTo(map);
+map.addControl(new mapboxgl.NavigationControl());
